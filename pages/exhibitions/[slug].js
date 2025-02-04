328657ba0353d7a4b05af39b9843e8bac7caf1ac
Rabe200 <rabenklar@gmx.de>
2025-02-04 12:43:32 +0100

    add artists to exhibition slug

--- pages/exhibitions/[slug].js
+++ pages/exhibitions/[slug].js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styled from "styled-components";
 import fs from "fs";
 import path from "path";
+import Link from "next/link";
 
 export async function getStaticPaths() {
   const filePath = path.join(process.cwd(), "data.json");
@@ -24,10 +25,24 @@ export async function getStaticProps({ params }) {
     };
   }
 
-  return { props: { exhibition } };
+  // Find participating artists
+  const artists = jsonData.artists.filter(
+    (artist) => artist.exhibition === exhibition.slug
+  );
+
+  return { props: { exhibition, artists } };
 }
 
-export default function ExhibitionPage({ exhibition }) {
+const ArtistList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const ArtistItem = styled.li`
+  margin: 5px 0;
+`;
+
+export default function ExhibitionPage({ exhibition, artists }) {
   return (
     <>
       <Head>
@@ -36,8 +51,24 @@ export default function ExhibitionPage({ exhibition }) {
       <h2>{exhibition.title}</h2>
       <p>{exhibition.description}</p>
       <p>
-        <strong>Date:</strong> {exhibition.date}
+        <strong>Vernissage:</strong> {exhibition.vernissage}
       </p>
+      <p>
+        <strong>Exhibition Duration:</strong> {exhibition.duration}
+      </p>
+      <p>
+        <strong>Location:</strong> {exhibition.location}
+      </p>
+
+      {/* List Participating Artists */}
+      <h3>Participating Artists</h3>
+      <ArtistList>
+        {artists.map((artist) => (
+          <ArtistItem key={artist.id}>
+            <Link href={`/artists/${artist.slug}`}>{artist.name}</Link>
+          </ArtistItem>
+        ))}
+      </ArtistList>
     </>
   );
 }
